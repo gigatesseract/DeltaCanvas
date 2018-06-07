43d8894c8afd21b490812307eec29d21daebb9d3
gigatesseract <38406487+gigatesseract@users.noreply.github.com>
2018-06-07 22:03:13 +0530

    normal mode (collision detect + reset + score)

--- dynamics.js
+++ dynamics.js
@@ -1,31 +1,60 @@
 var canvas = document.getElementById('canvas');
 var c = canvas.getContext('2d');
+var reset = document.getElementById('reset');
 var points = [];
+var gap = 100;
 
 var height, width, i;
-var length = 4;
+var length = 5;
 var speed = 3;
-var obj;
+var obj = {x:0, y:0};
 var x = 0, y = 0;
+var score;
+var gameflag = false;
+var check;
+height = canvas.height;
+width = canvas.width;
+function init(){
+  x = 25, y = 25;
+  speec = 3;
+  length = 5;
+  gap = 150;
+  score = 0;
+  gameflag = false;
+  check = false;
+  points.splice(0, length)
+    for(i=0;i<length;i++)
+  {
+    obj = {x:0, y:0};
+    obj.x = width + (width/length)*(i);
 
-var check = false;
+    obj.y = 0.1*height + (Math.random()*0.7*height);
+    points.push(obj);
+
+  }
+  canvas.style.cursor = "none";
+
+
+
+}
+// for(i=0;i<length;i++)
+// {
+//   var obj = {x:0, y:0};
+//   obj.x = width + (width/length)*(i);
+//
+//   obj.y = 0.1*height + (Math.random()*0.7*height);
+//   points.push(obj);
+//
+// }
+
+
 function resize(){
 canvas.width = window.innerWidth;
  canvas.height = window.innerHeight;
  height = canvas.height;
  width = canvas.width;
 }
-width = canvas.width;
-height= canvas.height;
-for(i=0;i<length;i++)
-{
-  obj = {x:0, y:0};
-  obj.x = width + (width/length)*(i);
 
-  obj.y = 0.1*height + (Math.random()*0.7*height);
-  points.push(obj);
-
-}
 
 
 function drawBackground(){
@@ -34,8 +63,8 @@ c.clearRect(0,0,canvas.width, canvas.height);
 
 for(i=0;i<length;i++){
 
-
-  if(points[i].x>0){
+ if(!gameflag){
+  if(points[i].x>0 ){
     points[i].x-=speed;
 
 
@@ -48,7 +77,7 @@ for(i=0;i<length;i++){
     points[i].y = 0.1*height + (Math.random()*0.7*height);
 
   }
-  c.lineWidth = "20";
+  c.lineWidth = "80";
   // c.arc(points[i].x, points[i].y, 10, 0, 2*Math.PI, false);
   // c.fill();
 c.beginPath();
@@ -57,81 +86,136 @@ c.lineTo(points[i].x, canvas.height);
 c.closePath();
 c.stroke();
 
-c.moveTo(points[i].x, points[i].y - 75);
+c.moveTo(points[i].x, points[i].y - gap);
 c.lineTo(points[i].x, 0);
 c.closePath();
 c.stroke();
+}
 }
 
 
 }
 
 function pathCheck(){
-console.log(x);
+
 var wall = new Path2D();
+
   for(i=0;i<length;i++)
   {
 
-
+  c.lineWidth = "40";
     wall.moveTo(points[i].x, points[i].y);
+    wall.lineTo(points[i].x + 40, points[i].y);
+    wall.lineTo(points[i].x+40, canvas.height);
     wall.lineTo(points[i].x, canvas.height);
+      wall.closePath();
+
+    wall.moveTo(points[i].x, points[i].y-gap);
+    wall.lineTo(points[i].x+40, points[i].y - gap);
+    wall.lineTo(points[i].x + 40, 0);
+    wall.lineTo(points[i].x,0);
     wall.closePath();
-    wall.moveTo(points[i].x, points[i].y-75);
-    wall.lineTo(points[i].x, 0);
-    wall.closePath();
-    c.lineWidth = "2";
-    c.stroke(wall);
 
 
   }
 
-    //if(x in xpoints)//(y<=points[i].y-150 && y >=0) || (y >=points[i].y && y<=canvas.height))
-    // check = xpoints.indexOf(e.pageX);
-    // if(check!=-1)
-    // {
-    //   if(!(y<=points[check].y && y>=points[check].y-75))
-    //   alert("boom");
-    // }
+if(c.isPointInPath(wall, x+40, y+15)){
 
+  gameflag = true;
 
+}
+else if(c.isPointInPath(wall, x, y)) gameflag = true;
+else if(c.isPointInPath(wall, x, y-10)) gameflag = true;
 
-
-//
-// if(check!=-1) alert('boom');
-
-if(c.isPointInPath(wall, x, y)) alert("boom");
 delete wall;
 
 }
-function drawDeep(){
 
+function gameOver(){
+  if(gameflag)
+  {
+  reset.style.display = "block";
+    canvas.style.cursor = "auto";
+    drawBackground();
+    drawDeep();
+    c.fillStyle = "rgba(100,100,100,0.3)";
+    c.fillRect(0,0,width, height);
+
+    c.font = "20px serif"
+    c.fillStyle = "red";
+    c.fillText("GAME OVER!!",  width/2-50, height/4);
+    c.fillStyle = "blue";
+    c.fillText("Score: " + score, width/2 - 50, height/3);
+  }
+  else{
+    reset.style.display = "none";
+  }
+
+}
+function drawDeep(){
+ if(!gameflag){
   c.beginPath();
+
   c.save();
+
   c.fillStyle = "red";
-   c.arc(x, y, 10, 0, 2*Math.PI, false);
+
+   c.arc(x, y, 15,0 , 2*Math.PI, false);
 
    c.fill();
    c.restore();
-   pathCheck();
+}
 }
 
 function mousemoveHandler(e){
 
-     x = e.pageX;// x == the location of the click in the document - the location (relative to the left) of the canvas in the document
+     x = e.pageX;
      y = e.pageY;
-pathCheck(e);
+// pathCheck(e);
 
 }
+
+function drawScore(){
+  c.fillStyle = "blue";
+  c.font = "20px Arial";
+  c.fillText("Score: "+ score, 30, 30);
+
+}
+
+function calculateScore(){
+  if(!gameflag)
+  score +=(speed*10)/3;
+  //window.setInterval(calculateScore, 10);
+}
+  window.setInterval(calculateScore, 100);
+
+  function resetHandler(){
+
+    init();
+    draw();
+
+
+  }
+
+
+reset.addEventListener('click', resetHandler);
 canvas.addEventListener('mousemove', mousemoveHandler);
 
 
+// window.setTimeout(calculateScore, 10);
 function draw(){
   resize();
+gameOver();
 
-drawBackground();
-
+if(!gameflag){
+  drawBackground();
+// calculateScore();
+drawScore();
 drawDeep();
-window.setTimeout(draw, 20);
-}
+window.requestAnimationFrame(pathCheck);
+window.requestAnimationFrame(draw);
 
+}
+}
+init();
 draw();
